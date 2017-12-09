025ceb0ae84aa3c83061f04a72683d9daeecc543
Tiago Bernardes Almeida <tiagoalmeida.95@hotmail.com>
2017-12-09 16:46:19 +0000

    Fixed graph and added getEdge to Vertex

--- src/java/graph/Graph.java
+++ src/java/graph/Graph.java
@@ -79,13 +79,12 @@ public class Graph {
         }
        
         //and that the Edge isn't already incident to one of the vertices
-        else if(one.containsNeighbor(e) || two.containsNeighbor(e)){
+        else if(one.containsNeighbor(e)){
             return false;
         }
             
         edges.put(e.hashCode(), e);
         one.addNeighbor(e);
-        two.addNeighbor(e);
         return true;
     }
     
