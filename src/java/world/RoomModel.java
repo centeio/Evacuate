838a4a7d6337438a3c788816ad672a7e9313c3eb
Tiago Bernardes Almeida <tiagoalmeida.95@hotmail.com>
2017-12-10 19:02:04 +0000

    Mobility probability fix

--- src/java/world/RoomModel.java
+++ src/java/world/RoomModel.java
@@ -21,6 +21,7 @@ public class RoomModel extends GridWorldModel {
 	public static final int FIRE = 32;
 	private static final int FIRESPREAD = 25;
 	private static final double MAXSPEED = 2.0;
+	private static final double MOBILITY = 5;
 	public static final Location MAINEXIT = new Location(Integer.MAX_VALUE, Integer.MAX_VALUE);
 
 	private static Random random = new Random(System.currentTimeMillis());
@@ -116,7 +117,7 @@ public class RoomModel extends GridWorldModel {
 				model.setAgPos(i, x, y);
 				model.panicscales.add(i, 0.0);
 				model.ishelping.add(i, -1);				
-				if(random.nextInt(100) < 50) {
+				if(random.nextInt(100) < MOBILITY) {
 					model.injscales.add(i, 0.1 + random.nextInt(2)/10.0);
 				}
 				else
